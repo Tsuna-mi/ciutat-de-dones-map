63d4c6dbd10c81f1af6d426117b693a9e4101a1f
Tsuna-mi <belen_g_romero@hotmail.com>
2023-12-09 23:33:55 +0100

    feat: modify theme file

--- src/theme.ts
+++ src/theme.ts
@@ -2,9 +2,26 @@ import { extendTheme, ThemeConfig } from '@chakra-ui/react';
 
 const config: ThemeConfig = {
   initialColorMode: 'system',
-  useSystemColorMode: false,
+  useSystemColorMode: true,
+  cssVarPrefix: 'ck',
 };
 
-const theme = extendTheme({ config });
+const theme = extendTheme({
+  config,
+  colors: {
+    primary: '#5A3B5D',
+    secondary: '#8B538F',
+    secondaryLight: '#C3A3C9',
+    accent: '#FEDC2A',
+    white: '#fff',
+    yellow: '#FCDF87',
+    gray5: '#555',
+    gray3: '#333',
+  },
+  fonts: {
+    heading: `'Kanit', sans-serif`,
+    body: `'Source Sans Pro', sans-serif`,
+  },
+});
 
 export default theme;
