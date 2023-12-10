69535f748e24a2d98e201c8a9c7844f6d5b5a217
Tsuna-mi <belen_g_romero@hotmail.com>
2023-12-10 17:55:42 +0100

    feat: create buttons variations for map

--- src/theme.ts
+++ src/theme.ts
@@ -22,6 +22,49 @@ const theme = extendTheme({
     heading: `'Kanit', sans-serif`,
     body: `'Source Sans Pro', sans-serif`,
   },
+  components: {
+    Button: {
+      baseStyle: {
+        fontWeight: 'bold',
+        fontSize: '2xl',
+        color: 'white',
+        textTransform: 'uppercase',
+        h: '45px',
+        border: '2px solid #C3A3C9',
+        bg: '8B538F',
+      },
+      sizes: {
+        lg: {
+          h: '45px',
+          fontSize: 'lg',
+          px: '32px',
+        },
+      },
+      variants: {
+        map: {
+          bg: '#8B538F',
+          color: '#FEDC2A',
+          _hover: {
+            bg: '#5A3B5D',
+            borderWidth: '4px',
+            _disabled: {
+              bg: '#555',
+              color: 'white',
+              borderWidth: '2px',
+            },
+          },
+          _disabled: {
+            bg: '#555',
+            color: 'white',
+          },
+        },
+      },
+      defaultProps: {
+        variant: 'solid',
+        size: 'lg',
+      },
+    },
+  },
 });
 
 export default theme;
