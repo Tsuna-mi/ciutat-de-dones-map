9c89f1b7b1ab0c8e6c9468b9b981dcc3e63987e4
Tsuna-mi <belen_g_romero@hotmail.com>
2023-12-11 23:36:37 +0100

    feat: create results list page

--- src/pages/Results/index.tsx
+++ src/pages/Results/index.tsx
@@ -1,9 +1,294 @@
+import {
+  Box,
+  Center,
+  Divider,
+  Flex,
+  Heading,
+  Image,
+  List,
+  ListItem,
+  Spacer,
+  Text,
+} from '@chakra-ui/react';
+import { ChevronDownIcon } from '@chakra-ui/icons';
 import { Page } from 'shared/Layout/Page';
+import { BackButton } from 'shared/Layout/BackButton';
 
 export function ResultsPage() {
   return (
-    <Page maxW="container.xl" spacing={{ base: 8, lg: 20 }}>
-      <p>Results Page</p>
+    <Page>
+      <Flex marginBottom="20px">
+        <BackButton />
+        <Spacer />
+        <Box height="45px" width="100px" bg="white" borderRadius={'6'}>
+          <Center>
+            <Text as="span" color="gray5" lineHeight="2.7rem">
+              {'28'}
+            </Text>
+          </Center>
+        </Box>
+      </Flex>
+      <Divider bg="white" />
+      <Center>
+        <ChevronDownIcon w={14} h={14} color="accent" />
+      </Center>
+      <List>
+        <ListItem>
+          <Box as="article">
+            <Flex>
+              <Box>
+                <Heading
+                  as="h2"
+                  fontFamily={'Source Sans Pro'}
+                  fontSize="24px"
+                  style={{
+                    whiteSpace: 'preserve',
+                  }}
+                >
+                  Valerie Powles
+                </Heading>
+                <Text
+                  fontFamily={'Source Sans Pro'}
+                  fontWeight={'100'}
+                  fontSize={'20px'}
+                  lineHeight={'1'}
+                  style={{
+                    whiteSpace: 'preserve',
+                  }}
+                >
+                  Birmingham, 1950 - Barcelona, 2011
+                </Text>
+              </Box>
+              <Image src={''} alt={''} />
+            </Flex>
+
+            <Box>
+              <Text
+                fontSize={'18px'}
+                style={{
+                  maxHeight: '80px',
+                  margin: '15px 0 20px',
+                  whiteSpace: 'preserve',
+                  overflow: 'hidden',
+                }}
+              >
+                Historiadora y activista por la conservación del patrimonio y la
+                memoria, cofundó el Centro de Investigación Histórica del
+                Poble-sec, el barrio donde vivió. Compaginó la docencia con el
+                trabajo reivindicativo, defendiendo la conservación de El Molino
+                cuando el inversor que compró el edificio se deshizo de la
+                decoración modernista diseñada por M. J. Raspall. Se movilizó
+                con el vecindario, consiguieron recuperar parte del patrimonio e
+                iniciaron una intensa campaña para salvar la sala. Historiadora
+                y activista por la conservación del patrimonio y la memoria,
+                cofundó el Centro de Investigación Histórica del Poble-sec, el
+                barrio donde vivió. Compaginó la docencia con el trabajo
+                reivindicativo, defendiendo la conservación de El Molino cuando
+                el inversor que compró el edificio se deshizo de la decoración
+                modernista diseñada por M. J. Raspall. Se movilizó con el
+                vecindario, consiguieron recuperar parte del patrimonio e
+                iniciaron una intensa campaña para salvar la sala.
+              </Text>
+            </Box>
+          </Box>
+          <Divider bg="white" />
+        </ListItem>
+        <ListItem>
+          <Box
+            as="article"
+            style={{
+              marginTop: '40px',
+            }}
+          >
+            <Flex>
+              <Box>
+                <Heading
+                  as="h2"
+                  fontFamily={'Source Sans Pro'}
+                  fontSize="24px"
+                  style={{
+                    whiteSpace: 'preserve',
+                  }}
+                >
+                  Valerie Powles
+                </Heading>
+                <Text
+                  fontFamily={'Source Sans Pro'}
+                  fontWeight={'100'}
+                  fontSize={'20px'}
+                  lineHeight={'1'}
+                  style={{
+                    whiteSpace: 'preserve',
+                  }}
+                >
+                  Birmingham, 1950 - Barcelona, 2011
+                </Text>
+              </Box>
+              <Image src={''} alt={''} />
+            </Flex>
+
+            <Box>
+              <Text
+                fontSize={'18px'}
+                style={{
+                  maxHeight: '80px',
+                  margin: '15px 0 20px',
+                  whiteSpace: 'preserve',
+                  overflow: 'hidden',
+                }}
+              >
+                Historiadora y activista por la conservación del patrimonio y la
+                memoria, cofundó el Centro de Investigación Histórica del
+                Poble-sec, el barrio donde vivió. Compaginó la docencia con el
+                trabajo reivindicativo, defendiendo la conservación de El Molino
+                cuando el inversor que compró el edificio se deshizo de la
+                decoración modernista diseñada por M. J. Raspall. Se movilizó
+                con el vecindario, consiguieron recuperar parte del patrimonio e
+                iniciaron una intensa campaña para salvar la sala. Historiadora
+                y activista por la conservación del patrimonio y la memoria,
+                cofundó el Centro de Investigación Histórica del Poble-sec, el
+                barrio donde vivió. Compaginó la docencia con el trabajo
+                reivindicativo, defendiendo la conservación de El Molino cuando
+                el inversor que compró el edificio se deshizo de la decoración
+                modernista diseñada por M. J. Raspall. Se movilizó con el
+                vecindario, consiguieron recuperar parte del patrimonio e
+                iniciaron una intensa campaña para salvar la sala.
+              </Text>
+            </Box>
+          </Box>
+          <Divider bg="white" />
+        </ListItem>
+        <ListItem>
+          <Box
+            as="article"
+            style={{
+              marginTop: '40px',
+            }}
+          >
+            <Flex>
+              <Box>
+                <Heading
+                  as="h2"
+                  fontFamily={'Source Sans Pro'}
+                  fontSize="24px"
+                  style={{
+                    whiteSpace: 'preserve',
+                  }}
+                >
+                  Valerie Powles
+                </Heading>
+                <Text
+                  fontFamily={'Source Sans Pro'}
+                  fontWeight={'100'}
+                  fontSize={'20px'}
+                  lineHeight={'1'}
+                  style={{
+                    whiteSpace: 'preserve',
+                  }}
+                >
+                  Birmingham, 1950 - Barcelona, 2011
+                </Text>
+              </Box>
+              <Image src={''} alt={''} />
+            </Flex>
+
+            <Box>
+              <Text
+                fontSize={'18px'}
+                style={{
+                  maxHeight: '80px',
+                  margin: '15px 0 20px',
+                  whiteSpace: 'preserve',
+                  overflow: 'hidden',
+                }}
+              >
+                Historiadora y activista por la conservación del patrimonio y la
+                memoria, cofundó el Centro de Investigación Histórica del
+                Poble-sec, el barrio donde vivió. Compaginó la docencia con el
+                trabajo reivindicativo, defendiendo la conservación de El Molino
+                cuando el inversor que compró el edificio se deshizo de la
+                decoración modernista diseñada por M. J. Raspall. Se movilizó
+                con el vecindario, consiguieron recuperar parte del patrimonio e
+                iniciaron una intensa campaña para salvar la sala. Historiadora
+                y activista por la conservación del patrimonio y la memoria,
+                cofundó el Centro de Investigación Histórica del Poble-sec, el
+                barrio donde vivió. Compaginó la docencia con el trabajo
+                reivindicativo, defendiendo la conservación de El Molino cuando
+                el inversor que compró el edificio se deshizo de la decoración
+                modernista diseñada por M. J. Raspall. Se movilizó con el
+                vecindario, consiguieron recuperar parte del patrimonio e
+                iniciaron una intensa campaña para salvar la sala.
+              </Text>
+            </Box>
+          </Box>
+          <Divider bg="white" />
+        </ListItem>
+        <ListItem>
+          <Box
+            as="article"
+            style={{
+              marginTop: '40px',
+            }}
+          >
+            <Flex>
+              <Box>
+                <Heading
+                  as="h2"
+                  fontFamily={'Source Sans Pro'}
+                  fontSize="24px"
+                  style={{
+                    whiteSpace: 'preserve',
+                  }}
+                >
+                  Valerie Powles
+                </Heading>
+                <Text
+                  fontFamily={'Source Sans Pro'}
+                  fontWeight={'100'}
+                  fontSize={'20px'}
+                  lineHeight={'1'}
+                  style={{
+                    whiteSpace: 'preserve',
+                  }}
+                >
+                  Birmingham, 1950 - Barcelona, 2011
+                </Text>
+              </Box>
+              <Image src={''} alt={''} />
+            </Flex>
+
+            <Box>
+              <Text
+                fontSize={'18px'}
+                style={{
+                  maxHeight: '80px',
+                  margin: '15px 0 20px',
+                  whiteSpace: 'preserve',
+                  overflow: 'hidden',
+                }}
+              >
+                Historiadora y activista por la conservación del patrimonio y la
+                memoria, cofundó el Centro de Investigación Histórica del
+                Poble-sec, el barrio donde vivió. Compaginó la docencia con el
+                trabajo reivindicativo, defendiendo la conservación de El Molino
+                cuando el inversor que compró el edificio se deshizo de la
+                decoración modernista diseñada por M. J. Raspall. Se movilizó
+                con el vecindario, consiguieron recuperar parte del patrimonio e
+                iniciaron una intensa campaña para salvar la sala. Historiadora
+                y activista por la conservación del patrimonio y la memoria,
+                cofundó el Centro de Investigación Histórica del Poble-sec, el
+                barrio donde vivió. Compaginó la docencia con el trabajo
+                reivindicativo, defendiendo la conservación de El Molino cuando
+                el inversor que compró el edificio se deshizo de la decoración
+                modernista diseñada por M. J. Raspall. Se movilizó con el
+                vecindario, consiguieron recuperar parte del patrimonio e
+                iniciaron una intensa campaña para salvar la sala.
+              </Text>
+            </Box>
+          </Box>
+          <Divider bg="white" />
+        </ListItem>
+      </List>
     </Page>
   );
 }
