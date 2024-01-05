418a94c98e8723acc116e9d72dd4f52b54628f3e
Tsuna-mi <belen_g_romero@hotmail.com>
2024-01-05 16:18:35 +0100

    feat: add new types and utils to sort and filter data

---
+++ src/app/types.d.ts
@@ -0,0 +1,47 @@
+export type Biography = {
+  id: number;
+  name: string;
+  birthDate: string | null;
+  dates: string;
+  alt: string;
+  description: string;
+  coords: string;
+  shape: 'circle' | 'rect';
+  image: string;
+  subwayLine: SubwayLine[];
+  category: Category[];
+};
+
+export type SortingAndDirection = {
+  sorting: string;
+  direction: 'asc' | 'desc';
+  subwayLine: string;
+  category: string;
+};
+
+export type Category =
+  | 'allCategories'
+  | 'activism'
+  | 'health'
+  | 'sports'
+  | 'culture'
+  | 'literature'
+  | 'rights'
+  | 'urbanism';
+
+export type SubwayLine =
+  | 'allSubwayLines'
+  | 'l1'
+  | 'l2'
+  | 'l3'
+  | 'l4'
+  | 'l5'
+  | 'l6'
+  | 'l7'
+  | 'l9s'
+  | 'l9n'
+  | 'l10s'
+  | 'l10n'
+  | 'l11'
+  | 'l12'
+  | 's1';
--- src/utils/general.ts
+++ src/utils/general.ts
@@ -1,7 +1,36 @@
 import { CreateStyled } from '@emotion/styled';
+import { Biography, SubwayLine, Category } from 'app/types';
 
 export const transientOptions: Parameters<CreateStyled>[1] = {
   shouldForwardProp: (propName: string) => !propName.startsWith('$'),
 };
 
-export default transientOptions;
+export const sortArrayByField = (
+  array: Biography[],
+  field: 'name' | 'birthDate',
+  method: 'asc' | 'desc'
+): Biography[] => {
+  return [...array].sort((a, b) => {
+    const aValue = a[field] || '';
+    const bValue = b[field] || '';
+    if (aValue < bValue) {
+      return method === 'asc' ? -1 : 1;
+    }
+    if (aValue > bValue) {
+      return method === 'asc' ? 1 : -1;
+    }
+    return 0;
+  });
+};
+
+export const filterByStringInArrayField = (
+  array: Biography[],
+  field: 'category' | 'subwayLine',
+  filterString: SubwayLine | Category
+): Biography[] => {
+  if (filterString === 'allSubwayLines' || filterString === 'allCategories')
+    return array;
+  return array.filter(
+    (item) => Array.isArray(item[field]) && item[field].includes(filterString)
+  );
+};
