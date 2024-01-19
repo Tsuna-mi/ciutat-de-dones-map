50381d8bf6d41064ab2a108efc2eddffac5a7057
Tsuna-mi <belen_g_romero@hotmail.com>
2024-01-19 21:09:37 +0100

    fix: require data before try

--- netlify/functions/bios.js
+++ netlify/functions/bios.js
@@ -1,6 +1,6 @@
 exports.handler = async (event, context) => {
+  const data = require('../../dist/data/bios.json');
   try {
-    const data = require('../../dist/data/bios.json');
     return {
       statusCode: 200,
       body: JSON.stringify(data),
