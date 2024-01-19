2dbac78068dd07b70cb12a83f01c894ce6f77009
Tsuna-mi <belen_g_romero@hotmail.com>
2024-01-19 20:56:28 +0100

    fix: remove unneeded dependencies and require json directly

--- netlify/functions/bios.js
+++ netlify/functions/bios.js
@@ -1,12 +1,6 @@
-import '../../dist/data/bios.json';
-const { readFileSync } = require('fs');
-const path = require('path');
-
 exports.handler = async (event, context) => {
   try {
-    const data = readFileSync(path.join(__dirname, '..', '..', 'dist', 'data', 'bios.json'));
-    console.log('Attempting to read file at:', path.join(__dirname, '..', '..', 'dist', 'data', 'bios.json'));
-    console.log('Current directory:', __dirname);
+    const data = require('../../dist/data/bios.json');
     return {
       statusCode: 200,
       body: data.toString(),
@@ -19,7 +13,7 @@ exports.handler = async (event, context) => {
     console.error('Error:', error);
     return {
       statusCode: 500,
-      body: error.toString(),
+      body: JSON.stringify({ message: 'Internal Server Error' }),
       headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*',
